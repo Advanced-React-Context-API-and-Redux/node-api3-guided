aa4cf9d5946b67131c0a00f7b7eefefb6f8cfcaf
seanaleid <seanaleid@gmail.com>
2020-03-11 14:04:56 -0400

    Add notes and functional code
      * Adds notes about middleware
      * Shows a few examples of how to setup a basic middleware and then one that returns a value
      * Adds functional versions of two different ways to use middleware to validate or add

--- server.js
+++ server.js
@@ -9,13 +9,19 @@ const server = express();
 
 // global middleware(applied to every request coming in to the server) - you do it by calling server.use() and pass the middleware as a parameter --> server.use(express.json())
 server.use(morgan('dev')); // third party, needs to be npm installed
-server.use(logger);
+// server.use(logger);
 server.use(helmet()); // use the middleware --> execute it 
 server.use(express.json()); // built-in middleware: no need to npm install
 
-server.use('/api/hubs', hubsRouter);
+server.use('/api/hubs', logger, hubsRouter);
 
-server.get('/', (req, res) => {
+// middleware doesn't have to return a value
+// middleware must either call next or produce a response
+
+// global use, if it's only for a few routes, we can place it locally
+// server.use(addName);
+
+server.get('/', addName, logger, (req, res) => {
   const nameInsert = (req.name) ? ` ${req.name}` : '';
 
   console.log("req.name is:", req.name)
@@ -26,8 +32,24 @@ server.get('/', (req, res) => {
     `);
 });
 
+function addName(req, res, next) {
+  // ultra, mega, super, supra, giga secret sauce
+  req.name = 'Web 27'
+
+  next();
+}
+
 server.use(function(req, res, next){
   res.status(404).json({ error: "Oops, didn't find what you're looking for."})
 })
 
 module.exports = server;
+
+// option for how to use  middleware that returns a value and how you have to change the structure.
+// function addName(name) {
+//   return function(req, res, next ) {
+//     req.name= name;
+
+//     next();
+//   };
+// }
